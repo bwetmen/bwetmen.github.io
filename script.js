59225c674e4284c28b508c493ec5f0e3db70e063
bwetmen <152396124+bwetmen@users.noreply.github.com>
2023-12-04 06:37:11 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function ubahteks()
+{
+    let nama = "Saya Albert Christian";
+    document.getElementById("keterangan").innerHTML = "Peserta DSF 2023";
+    document.getElementById("paragraf").innerHTML = "Jangan Lupa Bahagia";
+    document.getElementById("nama").innerHTML = nama;
+}
